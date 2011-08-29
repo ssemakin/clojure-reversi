03c921c1c629be0bce5ba7dab1a99357173f3a05
ssemakin <ssemakin@gmail.com>
2011-08-29 16:42:36 +0300

    In stats total time of the game is shown now as well.

--- src/clojurereversi/stats.clj
+++ src/clojurereversi/stats.clj
@@ -20,20 +20,30 @@
 (defn min-of-stones [color data] (apply min (data color)))
 (defn min-of-total [data] (apply min (catdata data)))
 
-(defn to-msec [number]
-  (/ (double number) 1000000.0))
+(defn duration-total [data] (apply + (catdata data)))
 
-(defn to-sec [number]
-  (/ (double number) 1000000000.0))
+(defn to-msec [nanosecs]
+  (/ (double nanosecs) 1000000.0))
 
-(defn frmt [number]
-  (let [secs (to-sec number)]
-    (if (< secs 1.0)
-      (format "%.2f msecs" (to-msec number))
-      (format "%.3f secs" secs))))
+(defn to-sec [nanosecs]
+  (/ (double nanosecs) 1000000000.0))
+
+(defn to-min [nanosecs]
+  (/ (double nanosecs) 1000000000.0 60.0))
+
+(defn frmt [nanosecs]
+  (let [secs (to-sec nanosecs)]
+    (cond
+      (> secs 60.0)
+        (format "%.2f mins" (to-min nanosecs))
+      (< secs 1.0)
+        (format "%.2f msecs" (to-msec nanosecs))
+      :else
+        (format "%.3f secs" secs))))
 
 (defn print-stats [data]
-  (println "\ntime per one move:")
+  (println "\ntotal duration of the game:" (frmt (duration-total data)))
+  (println "time per one move:")
   (printf "%10s %20s %20s %20s\n" "" "[white stones]" "[black stones]" "[all stones]")
   (printf "%10s %20s %20s %20s\n" "average:"
     (frmt (avg-of-stones 'white data))
