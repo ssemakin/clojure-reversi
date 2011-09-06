6f5959db5ecafd15b76f7da15485c9350a5108df
ssemakin <ssemakin@gmail.com>
2011-09-06 21:05:18 +0300

    Added macro to generate Board type with variable number of attributs to access board cells.

--- src/clojurereversi/boardb.clj
+++ src/clojurereversi/boardb.clj
@@ -1,5 +1,9 @@
 (ns clojurereversi.boardb)
 
+;(defn gen-cell-name [row column]
+;  (. clojure.lang.Symbol (intern (str "c" row column))))
+(defn gen-cell-name [row column] (symbol (str "c" row column)))
+
 (defrecord Cell [color neighbours])
 
 (defrecord WindRose [n ne e se s sw w nw])
@@ -12,5 +16,19 @@
 (defrecord NorthEastSector [n ne e])
 (defrecord EastSector [n ne e se s])
 
-(defn empty-board [size])
+(defn gen-board-type-cells [size]
+  (def nums (range 1 (inc size)))
+  (vec (for [r nums c nums] (gen-cell-name r c))))
 
+(defn gen-board-type-args [size]
+  (-> (gen-board-type-cells size)
+    (conj 'white-stones)
+    (conj 'black-stones)
+    (conj 'size)))
+
+(defmacro gen-board-type [size]
+  (let [name 'Board
+        args (gen-board-type-args size)]
+    `(defrecord ~name ~args)))
+
+(defn empty-board [size])
---
+++ src/clojurereversi/main.clj
@@ -0,0 +1,5 @@
+(ns clojurereversi.main
+  (:use [clojurereversi.boardb]))
+
+(println (gen-board-type 2))
+(println (gen-board-type-args 2))
