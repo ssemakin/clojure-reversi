0a69a86b68b015e5a7539d9c9eee86714954d6f3
ssemakin <ssemakin@gmail.com>
2011-09-11 19:42:48 +0300

    (empty-board size) function is finished. It generates the Board class at runtime with the pre-set number of cells and initializes it with the Cell objects that hold neighbours for each cell.

--- src/clojurereversi/boardb.clj
+++ src/clojurereversi/boardb.clj
@@ -4,13 +4,13 @@
 (defrecord Cell [color neighbours])
 
 (defrecord FullSector360 [n ne e se s sw w nw])
-(defrecord SouthEastSector45 [e se s])
+(defrecord SouthEastSector90 [e se s])
 (defrecord SouthSector180 [e se s sw w])
-(defrecord SouthWestSector45 [s sw w])
+(defrecord SouthWestSector90 [s sw w])
 (defrecord WestSector180 [n s sw w nw])
-(defrecord NorthWestSector45 [n w nw])
+(defrecord NorthWestSector90 [n w nw])
 (defrecord NorthSector180 [n ne e w nw])
-(defrecord NorthEastSector45 [n ne e])
+(defrecord NorthEastSector90 [n ne e])
 (defrecord EastSector180 [n ne e se s])
 
 (defn direction-type? [t d]
@@ -20,18 +20,18 @@
         t-directions (clojure.set/intersection t-fields full360)]
     (= directions t-directions)))
 
-(defn build-sector
+(defmacro build-sector
   ([d]
     (cond
-      (direction-type? FullSector360 d) (FullSector360. (:n d) (:ne d) (:e d) (:se d) (:s d) (:sw d) (:w d) (:nw d))
-      (direction-type? SouthEastSector45 d) (SouthEastSector45. (:e d) (:se d) (:s d))
-      (direction-type? SouthSector180 d) (SouthSector180. (:e d) (:se d) (:s d) (:sw d) (:w d))
-      (direction-type? SouthWestSector45 d) (SouthWestSector45. (:s d) (:sw d) (:w d))
-      (direction-type? WestSector180 d) (WestSector180. (:n d) (:s d) (:sw d) (:w d) (:nw d))
-      (direction-type? NorthWestSector45 d) (NorthWestSector45. (:n d) (:w d) (:nw d))
-      (direction-type? NorthSector180 d) (NorthSector180. (:n d) (:ne d) (:e d) (:w d) (:nw d))
-      (direction-type? NorthEastSector45 d) (NorthEastSector45. (:n d) (:ne d) (:e d))
-      (direction-type? EastSector180 d) (EastSector180. (:n d) (:ne d) (:e d) (:se d) (:s d))
+      (direction-type? FullSector360 d) `(FullSector360. ~(:n d) ~(:ne d) ~(:e d) ~(:se d) ~(:s d) ~(:sw d) ~(:w d) ~(:nw d))
+      (direction-type? SouthEastSector90 d) `(SouthEastSector90. ~(:e d) ~(:se d) ~(:s d))
+      (direction-type? SouthSector180 d) `(SouthSector180. ~(:e d) ~(:se d) ~(:s d) ~(:sw d) ~(:w d))
+      (direction-type? SouthWestSector90 d) `(SouthWestSector90. ~(:s d) ~(:sw d) ~(:w d))
+      (direction-type? WestSector180 d) `(WestSector180. ~(:n d) ~(:s d) ~(:sw d) ~(:w d) ~(:nw d))
+      (direction-type? NorthWestSector90 d) `(NorthWestSector90. ~(:n d) ~(:w d) ~(:nw d))
+      (direction-type? NorthSector180 d) `(NorthSector180. (:n d) ~(:ne d) ~(:e d) ~(:w d) ~(:nw d))
+      (direction-type? NorthEastSector90 d) `(NorthEastSector90. ~(:n d) ~(:ne d) ~(:e d))
+      (direction-type? EastSector180 d) `(EastSector180. ~(:n d) ~(:ne d) ~(:e d) ~(:se d) ~(:s d))
       :else (throw (new Exception (str "Unknown direction type!! " d))))))
 
 (def dirs {:n north-dir :ne north-east-dir :e east-dir :se south-east-dir
@@ -40,25 +40,23 @@
 (defn find-neighbours [size [r c]]
   (reduce merge (keep #(when (valid-pos? size ((val %) [r c])) {(key %) ((val %) [r c])}) dirs)))
 
-;(defn gen-cell-name [row column]
-;  (. clojure.lang.Symbol (intern (str "c" row column))))
 (defn gen-cell-name [row column] (symbol (str "c" row column)))
 
 (defn gen-board-type-cells [size]
   (def nums (range 1 (inc size)))
   (vec (for [r nums c nums] (gen-cell-name r c))))
 
-(defn init-board-cells [size]
+(defmacro init-board-cells [size]
   (def nums (range 1 (inc size)))
   (vec (for [r nums c nums]
-         (Cell. nil (build-sector (find-neighbours size [r c]))))))
+         `(Cell. nil ~(macroexpand-1 `(build-sector ~(find-neighbours size [r c])))))))
 
 (defn gen-board-type-args [size]
   (-> (gen-board-type-cells size)
     (conj 'white-stones) (conj 'black-stones) (conj 'size)))
 
-(defn init-board-args [size]
-  (-> (init-board-cells size)
+(defmacro init-board-args [size]
+  (-> (macroexpand-1 `(init-board-cells ~size))
     (conj #{}) (conj #{}) (conj size)))
 
 (defmacro gen-board-type [size]
@@ -66,19 +64,11 @@
         args (gen-board-type-args size)]
     `(defrecord ~name ~args)))
 
-(defmacro new-obj [o-type & ctr-values]
-  `(new ~(eval o-type) ~@(flatten (eval (vec ctr-values)))))
-
-(defn make-board [size]
-  (let [board-args (init-board-args size);(zipmap (gen-board-type-cells size) (init-board-cells size)))
-      board (new-obj (gen-board-type 2) (vec (repeat (count board-args) 0)))]
-  )
-;  (new-obj (gen-board-type 2) (vec (repeat (count board-args) 0)))
-;  (reduce #(assoc %1 (key %2) (val %2)) board board-args)
-  )
+(defn empty-board [size]
+  (def board-type (eval (macroexpand-1 `(gen-board-type ~size))))
+  (def board-args (macroexpand-1 `(init-board-args ~size)))
+  (eval (macroexpand-1 `(new ~board-type ~@board-args))))
 
 ;(defn change-color [board [r c] color]
 ;  (assoc board (gen-cell-name r c)
 ;    (Cell. color )))
-
-(defn empty-board [size])
--- src/clojurereversi/main.clj
+++ src/clojurereversi/main.clj
@@ -1,28 +1,4 @@
 (ns clojurereversi.main
   (:use [clojurereversi.boardb]))
 
-(println (gen-board-type-args 2))
-
-(def b-class (gen-board-type 2))
-(println b-class)
-
-(def b (new-obj (gen-board-type 2) 1 2 3 4 5 6 7))
-(println b)
-(println ((keyword (gen-cell-name 1 2)) b))
-
-(def d { :n [1 2], :ne [1 2], :e [3 4], :se [5 6], :s [7 8] })
-(println (direction-type? clojurereversi.boardb.EastSector180 d))
-
-(println (build-sector {:e [1 2] :se [3 4] :s [5 6]}))
-;(println (build-sector {:e [1 2] :se [3 4] :s [5 6] :w [1 3]}))
-
-(def nbrs (find-neighbours 3 [1 1]))
-(println "neighbours for [1 1]:" nbrs "-->" (build-sector nbrs))
-;(println (assoc (build-sector nbrs) :e 1))
-
-(println (init-board-args 2))
-
-(println (macroexpand-1 '(new-obj (gen-board-type 2) (init-board-args 2))))
-(println (new-obj (gen-board-type 2) (init-board-args 2)))
-
-(println (make-board 2))
+(println (empty-board 2))
